4895627bd139875c49ce893c2959e9c4e570b47c
Shariq Raza Qadri <75565639+cosmicqbit@users.noreply.github.com>
2023-09-15 11:55:39 +0530

    Update V1-EC2.tf

--- Terraform/V1-EC2.tf
+++ Terraform/V1-EC2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+    ami = "ami-0f5ee92e2d63afc18"
     instance_type = "t2.micro"
     key_name = "dpp"
 } 
